e6ad6e5a83f355e0b0aa7f553d213aab8f303a4b
sakomoko <sakomoko@gmail.com>
2013-12-08 21:23:49 +0900

    最初のコミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuildParty::Application.config.secret_key_base = '23f6ee9edd5ac1bce6b4641235ba9b300f2862dca447069abe86211e5c0487daa3d9aae0d818c670352eac071ad691e6c22fd3251ede428b9b8aaf80c97b952f'
